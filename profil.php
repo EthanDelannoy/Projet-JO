766e4a41f363ed894185675026f8c03c03bbb47e
EthanDelannoy <ethan.delannoy12@gmail.com>
2024-06-24 16:27:26 +0200

    24-06-24

--- profil.php
+++ profil.php
@@ -1,17 +1,27 @@
-<?php ob_start(); ?>
+<?php
+require_once 'dbConnect.php';
+session_start();
 
+if (!isset($_SESSION['idUtilisateur'])) {
+    header('Location: login.php');
+    exit();
+}
 
+$pdo = getPDOConnexion();
+$stmt = $pdo->prepare('SELECT nom, prenom, email FROM UTILISATEUR WHERE idUtilisateur = ?');
+$stmt->execute([$_SESSION['idUtilisateur']]);
+$utilisateur = $stmt->fetch(PDO::FETCH_ASSOC);
 
-
-
+ob_start();
+?>
 
 
 
 <?php
-    $content = ob_get_clean();
-    $title = "Votre profil - Jeux Olympique - Handball";
-    $image = '<img class="fondAccueil" src="./public/css/image/fond profil.jpg" alt="Handball">';
-    $titre = "MON COMPTE";
-    require "template.php";
-    ?>
-  
+$content = ob_get_clean();
+$title = "Votre profil - Jeux Olympique - Handball";
+$image = '<img class="fondAccueil" src="./public/css/image/fond profil.jpg" alt="Handball">';
+$titre = 'MON COMPTE';
+require 'template.php';
+?>
+
