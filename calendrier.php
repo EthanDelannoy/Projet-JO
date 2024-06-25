f2c4b0a61ee587272cd6ae563a859e9f1f9295b6
EthanDelannoy <ethan.delannoy12@gmail.com>
2024-06-25 13:21:29 +0200

    25-06-24

--- calendrier.php
+++ calendrier.php
@@ -1,9 +1,375 @@
 <?php ob_start(); ?>
 
+<section class="pageCalendrier">
+<img class="mascotteCalendrier" src="./public/css/image/mascotte 1.png" alt="Mascotte">
+
+<div class="groupeParagrapheCalendrier">
+<p class="paragraphePageCalendrier">
+AUX JEUX OLYMPIQUE DE PARIS 2024, LES TOURNOIS DE HANDBALL DÉBUTERONT DÈS LE JEUDI 25 JUILLET, LA VEILLE DE LA CÉRÉMONIE D'OUVRETURE.
+</p>
+
+<p class="paragraphePageCalendrier2">
+LES EXPLOITS DES HANDBALLEURS VONT RYHTMER LES JEUX AVEC PAS MOINS DE 76 MATCHS PRÉVUS JUSQU'AU DIMANCHE 11 AOÛt, JOUR DE LA CÉRÉMONIE DE CLÔTURE DE PARIS 2024.
 
 
+<p class="paragraphePageCalendrier3">
+POUR TENTER DE VOUS PROCURER DES BILLETS POUR LE HANDBALL À PARIS 2024 CLIQUEZ SUR LE BOUTON CI-DESSOUS
+</p>
 
 
+<a href="./billeterie.php"><button class="boutonCalendrier">ACHETEZ VOS PLACE</button></a>
+</div>
+
+<img class="mascotteCalendrierTour" src="./public/css/image/TOUR_EIFFEL_MASCOTTE_OLY_2D_RVB 1.png" alt="MascotteTourEiffel">
+</section>
+
+<img class="tourEiffeljoCalendrier" src="./public/css/image/fond 2 calendrier 1.png" alt="TourEiffelJO">
+
+<section id="matchCalendrierHand">
+<p class="programmeCalendrier">
+    PROGRAMME DU HANDBALL AUX JO DE PARIS 2024
+</p>
+
+<p class="dateCalendrier">JEUDI 25 JUILLET</p>
+<p class="heureCalendrier">DEBUT : 9h00 FIN : 12h30 ARENA PARIS SUD 6</p>
+<div class="groupeMatchCalendrier">
+<p class="matchCalendrier">FEMMES, TOUR PRÉLIMINAIRE :<br></p>
+<ul>
+    <li>9H00 : SLOVÉNIE - DANEMARK</li>
+    <li>11H00 : PAYS-BAS - ANGOLA</li>
+</ul>
+</div>
+
+<p class="heureCalendrier">DEBUT : 14h00 FIN : 17h30 ARENA PARIS SUD 6</p>
+<div class="groupeMatchCalendrier">
+<p class="matchCalendrier">FEMMES, TOUR PRÉLIMINAIRE :<br></p>
+<ul>
+    <li>14H00 : ESPAGNE - BRÉSEIL</li>
+    <li>16H00 : ALLEMAGNE - RÉPUBLIQUE DE CORÉE</li>
+</ul>
+</div>
+
+<p class="heureCalendrier">DEBUT : 19h00 FIN : 22h30 ARENA PARIS SUD 6</p>
+<div class="groupeMatchCalendrier">
+<p class="matchCalendrier">FEMMES, TOUR PRÉLIMINAIRE :<br></p>
+<ul>
+    <li>19H00 : HONGRIE - FRANCE</li>
+    <li>21H00 : NORVÈGE - SUÈDE</li>
+</ul>
+</div>
+
+<p class="dateCalendrier">SAMEDI 27 JUILLET</p>
+<p class="heureCalendrier">DEBUT : 9h00 FIN : 12h30 ARENA PARIS SUD 6</p>
+<div class="groupeMatchCalendrier">
+<p class="matchCalendrier">HOMMES, TOUR PRÉLIMINAIRE :<br></p>
+<ul>
+    <li>9H00 : ESPAGNE - SLOVÉNIE</li>
+    <li>11H00 : HONGRIE - ÉGYPTE</li>
+</ul>
+</div>
+
+<p class="heureCalendrier">DEBUT : 14h00 FIN : 17h30 ARENA PARIS SUD 6</p>
+<div class="groupeMatchCalendrier">
+<p class="matchCalendrier">HOMMES, TOUR PRÉLIMINAIRE :<br></p>
+<ul>
+    <li>14H00 : CROATIE - JAPON</li>
+    <li>16H00 : NORVÈGE - AGENTINE</li>
+</ul>
+</div>
+
+<p class="heureCalendrier">DEBUT : 19h00 FIN : 22h30 ARENA PARIS SUD 6</p>
+<div class="groupeMatchCalendrier">
+<p class="matchCalendrier">HOMMES, TOUR PRÉLIMINAIRE :<br></p>
+<ul>
+    <li>19H00 : ALLEMAGNE - SUÈDE</li>
+    <li>21H00 : DANEMARK - FRANCE</li>
+</ul>
+</div>
+
+<p class="dateCalendrier">DIMANCHE 28 JUILLET</p>
+<p class="heureCalendrier">DEBUT : 9h00 FIN : 12h30 ARENA PARIS SUD 6</p>
+<div class="groupeMatchCalendrier">
+<p class="matchCalendrier">FEMMES, TOUR PRÉLIMINAIRE :<br></p>
+<ul>
+    <li>9H00 : BRÉSIL - HONGRIE</li>
+    <li>11H00 : RÉPUBLIQUE DE CORÉE - SLOVÉNIE</li>
+</ul>
+</div>
+
+<p class="heureCalendrier">DEBUT : 14h00 FIN : 17h30 ARENA PARIS SUD 6</p>
+<div class="groupeMatchCalendrier">
+<p class="matchCalendrier">FEMMES, TOUR PRÉLIMINAIRE :<br></p>
+<ul>
+    <li>14H00 : SUÈDE - ALLEMAGNE</li>
+    <li>16H00 : DANEMARK - NORVÈGE</li>
+</ul>
+</div>
+
+<p class="heureCalendrier">DEBUT : 19h00 FIN : 22h30 ARENA PARIS SUD 6</p>
+<div class="groupeMatchCalendrier">
+<p class="matchCalendrier">FEMMES, TOUR PRÉLIMINAIRE :<br></p>
+<ul>
+    <li>19H00 : ANGOLA - ESPAGNE</li>
+    <li>21H00 : FRANCE - PAYS-BAS</li>
+</ul>
+</div>
+
+<p class="dateCalendrier">LUNDI 29 JUILLET</p>
+<p class="heureCalendrier">DEBUT : 9h00 FIN : 12h30 ARENA PARIS SUD 6</p>
+<div class="groupeMatchCalendrier">
+<p class="matchCalendrier">HOMMES, TOUR PRÉLIMINAIRE :<br></p>
+<ul>
+    <li>9H00 : JAPON - ALLEMAGNE</li>
+    <li>11H00 : SLOVÉNIE - CROATIE</li>
+</ul>
+</div>
+
+<p class="heureCalendrier">DEBUT : 14h00 FIN : 17h30 ARENA PARIS SUD 6</p>
+<div class="groupeMatchCalendrier">
+<p class="matchCalendrier">HOMMES, TOUR PRÉLIMINAIRE :<br></p>
+<ul>
+    <li>14H00 : ÉGYPTE - DANEMARK</li>
+    <li>16H00 : SUÈDE - ESPAGNE</li>
+</ul>
+</div>
+
+<p class="heureCalendrier">DEBUT : 19h00 FIN : 22h30 ARENA PARIS SUD 6</p>
+<div class="groupeMatchCalendrier">
+<p class="matchCalendrier">HOMMES, TOUR PRÉLIMINAIRE :<br></p>
+<ul>
+    <li>19H00 : FRANCE - NORVÈGE</li>
+    <li>21H00 : ARGENTINE - HONGRIE</li>
+</ul>
+</div>
+
+<p class="dateCalendrier">MARDI 30 JUILLET</p>
+<p class="heureCalendrier">DEBUT : 9h00 FIN : 12h30 ARENA PARIS SUD 6</p>
+<div class="groupeMatchCalendrier">
+<p class="matchCalendrier">FEMMES, TOUR PRÉLIMINAIRE :<br></p>
+<ul>
+    <li>9H00 : ALLEMAGNE - SLOVÉNIE</li>
+    <li>11H00 : NORVÈGE - RÉPUBLIQUE DE CORÉE</li>
+</ul>
+</div>
+
+<p class="heureCalendrier">DEBUT : 14h00 FIN : 17h30 ARENA PARIS SUD 6</p>
+<div class="groupeMatchCalendrier">
+<p class="matchCalendrier">FEMMES, TOUR PRÉLIMINAIRE :<br></p>
+<ul>
+    <li>14H00 : PAYS-BAS - ESPAGNE</li>
+    <li>16H00 : HONGRIE - ANGOLA</li>
+</ul>
+</div>
+
+<p class="heureCalendrier">DEBUT : 19h00 FIN : 22h30 ARENA PARIS SUD 6</p>
+<div class="groupeMatchCalendrier">
+<p class="matchCalendrier">FEMMES, TOUR PRÉLIMINAIRE :<br></p>
+<ul>
+    <li>19H00 : FRANCE - BRÉSIL</li>
+    <li>21H00 : SUÈDE - DANEMARK</li>
+</ul>
+</div>
+
+<p class="dateCalendrier">MERCREDI 31 JUILLET</p>
+<p class="heureCalendrier">DEBUT : 9h00 FIN : 12h30 ARENA PARIS SUD 6</p>
+<div class="groupeMatchCalendrier">
+<p class="matchCalendrier">HOMMES, TOUR PRÉLIMINAIRE :<br></p>
+<ul>
+    <li>9H00 : NORVÈGE - HONGRIE</li>
+    <li>11H00 : CROATIE - ALLEMAGNE</li>
+</ul>
+</div>
+
+<p class="heureCalendrier">DEBUT : 14h00 FIN : 17h30 ARENA PARIS SUD 6</p>
+<div class="groupeMatchCalendrier">
+<p class="matchCalendrier">HOMMES, TOUR PRÉLIMINAIRE :<br></p>
+<ul>
+    <li>14H00 : ESPAGNE - JAPON</li>
+    <li>16H00 : SLOVÉNIE - SUÈDE</li>
+</ul>
+</div>
+
+<p class="heureCalendrier">DEBUT : 19h00 FIN : 22h30 ARENA PARIS SUD 6</p>
+<div class="groupeMatchCalendrier">
+<p class="matchCalendrier">HOMMES, TOUR PRÉLIMINAIRE :<br></p>
+<ul>
+    <li>19H00 : FRANCE - BRÉSIL</li>
+    <li>21H00 : SUÈDE - DANEMARK</li>
+</ul>
+</div>
+
+<p class="dateCalendrier">JEUDI 1ER AOÛT </p>
+<p class="heureCalendrier">DEBUT : 9h00 FIN : 12h30 ARENA PARIS SUD 6</p>
+<div class="groupeMatchCalendrier">
+<p class="matchCalendrier">FEMMES, TOUR PRÉLIMINAIRE :<br></p>
+<ul>
+    <li>9H00 : PAYS-BAS - BRÉSIL</li>
+    <li>11H00 : RÉPUBLIQUE DE CORÉE - SUÈDE</li>
+</ul>
+</div>
+
+<p class="heureCalendrier">DEBUT : 14h00 FIN : 17h30 ARENA PARIS SUD 6</p>
+<div class="groupeMatchCalendrier">
+<p class="matchCalendrier">FEMMES, TOUR PRÉLIMINAIRE :<br></p>
+<ul>
+    <li>14H00 : ESPAGNE - HONGRIE</li>
+    <li>16H00 : ANGOLA - FRANCE</li>
+</ul>
+</div>
+
+<p class="heureCalendrier">DEBUT : 19h00 FIN : 22h30 ARENA PARIS SUD 6</p>
+<div class="groupeMatchCalendrier">
+<p class="matchCalendrier">FEMMES, TOUR PRÉLIMINAIRE :<br></p>
+<ul>
+    <li>19H00 : ALLEMAGNE - DANEMARK</li>
+    <li>21H00 : SLOVÉNIE - NORVÈGE</li>
+</ul>
+</div>
+
+<p class="dateCalendrier">VENDREDI 2 AOÛT </p>
+<p class="heureCalendrier">DEBUT : 9h00 FIN : 12h30 ARENA PARIS SUD 6</p>
+<div class="groupeMatchCalendrier">
+<p class="matchCalendrier">HOMMES, TOUR PRÉLIMINAIRE :<br></p>
+<ul>
+    <li>9H00 : HONGRIE - DANEMARK</li>
+    <li>11H00 : ARGENTINE - FRANCE</li>
+</ul>
+</div>
+
+<p class="heureCalendrier">DEBUT : 14h00 FIN : 17h30 ARENA PARIS SUD 6</p>
+<div class="groupeMatchCalendrier">
+<p class="matchCalendrier">HOMMES, TOUR PRÉLIMINAIRE :<br></p>
+<ul>
+    <li>14H00 : CROATIE - SUÈDE</li>
+    <li>16H00 : ALLEMAGNE - ESPAGNE</li>
+</ul>
+</div>
+
+<p class="heureCalendrier">DEBUT : 19h00 FIN : 22h30 ARENA PARIS SUD 6</p>
+<div class="groupeMatchCalendrier">
+<p class="matchCalendrier">HOMMES, TOUR PRÉLIMINAIRE :<br></p>
+<ul>
+    <li>19H00 : JAPON - SLOVÉNIE</li>
+    <li>21H00 : NORVÈGE - ÉGYPTE</li>
+</ul>
+</div>
+
+<p class="dateCalendrier">SAMEDI 3 AOÛT </p>
+<p class="heureCalendrier">DEBUT : 9h00 FIN : 12h30 ARENA PARIS SUD 6</p>
+<div class="groupeMatchCalendrier">
+<p class="matchCalendrier">FEMMES, TOUR PRÉLIMINAIRE :<br></p>
+<ul>
+    <li>9H00 : HONGRIE - PAYS-BAS</li>
+    <li>11H00 : ESPAGNE - FRANCE</li>
+</ul>
+</div>
+
+<p class="heureCalendrier">DEBUT : 14h00 FIN : 17h30 ARENA PARIS SUD 6</p>
+<div class="groupeMatchCalendrier">
+<p class="matchCalendrier">FEMMES, TOUR PRÉLIMINAIRE :<br></p>
+<ul>
+    <li>14H00 : BRÉSIL - ANGOLA</li>
+    <li>16H00 : SLOVÉNIE - SUÈDE</li>
+</ul>
+</div>
+
+<p class="heureCalendrier">DEBUT : 19h00 FIN : 22h30 ARENA PARIS SUD 6</p>
+<div class="groupeMatchCalendrier">
+<p class="matchCalendrier">FEMMES, TOUR PRÉLIMINAIRE :<br></p>
+<ul>
+    <li>19H00 : NORVÈGE - ALLEMAGNE</li>
+    <li>21H00 : DANEMARK - RÉPUBLIQUE DE CORÉE</li>
+</ul>
+</div>
+
+<p class="dateCalendrier">DIMANCHE 4 AOÛT </p>
+<p class="heureCalendrier">DEBUT : 9h00 FIN : 12h30 ARENA PARIS SUD 6</p>
+<div class="groupeMatchCalendrier">
+<p class="matchCalendrier">HOMMES, TOUR PRÉLIMINAIRE :<br></p>
+<ul>
+    <li>9H00 : SUÈDE - JAPON</li>
+    <li>11H00 : ÉGYPTE - ARGENTINE</li>
+</ul>
+</div>
+
+<p class="heureCalendrier">DEBUT : 14h00 FIN : 17h30 ARENA PARIS SUD 6</p>
+<div class="groupeMatchCalendrier">
+<p class="matchCalendrier">HOMMES, TOUR PRÉLIMINAIRE :<br></p>
+<ul>
+    <li>14H00 : ALLEMAGNE - SLOVÉNIE</li>
+    <li>16H00 : HONGRIE - FRANCE</li>
+</ul>
+</div>
+
+<p class="heureCalendrier">DEBUT : 19h00 FIN : 22h30 ARENA PARIS SUD 6</p>
+<div class="groupeMatchCalendrier">
+<p class="matchCalendrier">HOMMES, TOUR PRÉLIMINAIRE :<br></p>
+<ul>
+    <li>19H00 : DANEMARK - NORVÈGE</li>
+    <li>21H00 : ESPAGNE - CROATIE</li>
+</ul>
+</div>
+
+<p class="dateCalendrier">MARDI 6 AOÛT </p>
+<p class="heureCalendrier">DEBUT : 9h30 FIN : 11h30 STADE PIERRE MAUROY</p>
+<p class="matchFinal">FEMMES, QUART DE FINALE<br></p>
+
+<p class="heureCalendrier">DEBUT : 13h30 FIN : 15h30 STADE PIERRE MAUROY</p>
+<p class="matchFinal">FEMMES, QUART DE FINALE<br></p>
+
+<p class="heureCalendrier">DEBUT : 17h30 FIN : 19h30 STADE PIERRE MAUROY</p>
+<p class="matchFinal">FEMMES, QUART DE FINALE<br></p>
+
+<p class="heureCalendrier">DEBUT : 21h30 FIN : 23h30 STADE PIERRE MAUROY</p>
+<p class="matchFinal">FEMMES, QUART DE FINALE<br></p>
+
+<p class="dateCalendrier">MERCREDI 7 AOÛT </p>
+<p class="heureCalendrier">DEBUT : 9h30 FIN : 11h30 STADE PIERRE MAUROY</p>
+<p class="matchFinal">HOMMES, QUART DE FINALE<br></p>
+
+<p class="heureCalendrier">DEBUT : 13h30 FIN : 15h30 STADE PIERRE MAUROY</p>
+<p class="matchFinal">HOMMES, QUART DE FINALE<br></p>
+
+<p class="heureCalendrier">DEBUT : 17h30 FIN : 19h30 STADE PIERRE MAUROY</p>
+<p class="matchFinal">HOMMES, QUART DE FINALE<br></p>
+
+<p class="heureCalendrier">DEBUT : 21h30 FIN : 23h30 STADE PIERRE MAUROY</p>
+<p class="matchFinal">HOMMES, QUART DE FINALE<br></p>
+
+<p class="dateCalendrier">JEUDI 8 AOÛT </p>
+<p class="heureCalendrier">DEBUT : 16h30 FIN : 18h30 STADE PIERRE MAUROY</p>
+<p class="matchFinal">FEMMES, DEMI-FINALE<br></p>
+
+<p class="heureCalendrier">DEBUT : 21h30 FIN : 23h30 STADE PIERRE MAUROY</p>
+<p class="matchFinal">FEMMES, DEMI-FINALE<br></p>
+
+<p class="dateCalendrier">VENDREDI 9 AOÛT </p>
+<p class="heureCalendrier">DEBUT : 16h30 FIN : 8h30 STADE PIERRE MAUROY</p>
+<p class="matchFinal">HOMMES, DEMI-FINALE<br></p>
+
+<p class="heureCalendrier">DEBUT : 21h30 FIN : 23h30 STADE PIERRE MAUROY</p>
+<p class="matchFinal">HOMMES, DEMI-FINALE<br></p>
+
+<p class="dateCalendrier">SAMEDI 10 AOÛT </p>
+<p class="heureCalendrier">DEBUT : 10h00 FIN : 12h00 STADE PIERRE MAUROY</p>
+<p class="matchFinal">FEMMES, MATCH POUR LA MÉDAILLE DE BRONZE<br></p>
+
+<p class="heureCalendrier">DEBUT : 15h00 FIN : 17h30 STADE PIERRE MAUROY</p>
+<p class="matchFinal">FEMMES, MATCH POUR LA MÉDAILLE D'OR<br></p>
+
+<p class="dateCalendrier">DIMANCHE 11 AOÛT </p>
+<p class="heureCalendrier">DEBUT : 9h00 FIN : 11h00 STADE PIERRE MAUROY</p>
+<p class="matchFinal">HOMMES, MATCH POUR LA MÉDAILLE DE BRONZE<br></p>
+
+<p class="heureCalendrier">DEBUT : 13h30 FIN : 16h00 STADE PIERRE MAUROY</p>
+<p class="matchFinal">HOMMES, MATCH POUR LA MÉDAILLE D'OR<br></p>
+
+<div class="divBoutonFinCalendrier">
+<a href="./billeterie.php"><button class="boutonCalendrier">ACHETEZ VOS PLACE</button></a>
+</div>
+</section>
+
+<a href="./calendrier.php"><img class="vector" src="./public/css/image/Vector.png" alt="Vector"></a>
 
 
 
--- public/css/js/script.js
+++ public/css/js/script.js
@@ -57,4 +57,3 @@ function validateForm() {
 
     return true;
 }
-
