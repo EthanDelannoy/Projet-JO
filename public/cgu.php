11a16f603a9ff60677847f822b47555f41259034
EthanDelannoy <ethan.delannoy12@gmail.com>
2024-07-16 13:06:21 +0200

    16-07-24

--- public/cgu.php
+++ public/cgu.php
@@ -1,7 +1,94 @@
 <?php ob_start(); ?>
 
 <section class="pageCgu">
-<h2>Conditions générales d'utilisation</h2>
+    <p class="grandTitreCgu">Conditions générales d'utilisation</h2>
+
+    <p  class="cguText">Les présentes conditions générales d'utilisation (dites « CGU ») ont pour objet l'encadrement juridique des modalités de mise à disposition du site et des services par _______________ et de définir les conditions d’accès et d’utilisation des services par « l'Utilisateur ».</p>
+
+    <p  class="cguText">Les présentes CGU sont accessibles sur le site à la rubrique «CGU».</p>
+
+    <p  class="cguText">Toute inscription ou utilisation du site implique l'acceptation sans aucune réserve ni restriction des présentes CGU par l’utilisateur. Lors de l'inscription sur le site via le Formulaire d’inscription, chaque utilisateur accepte expressément les présentes CGU en cochant la case précédant le texte suivant : « Je reconnais avoir lu et compris les CGU et je les accepte ».
+        En cas de non-acceptation des CGU stipulées dans le présent contrat, l'Utilisateur se doit de renoncer à l'accès des services proposés par le site.
+        http://jeuxolypmpique/ se réserve le droit de modifier unilatéralement et à tout moment le contenu des présentes CGU.
+    </p>
+
+    <p class="titreCgu">Article 1 : Les mentions légales</p>
+
+    <p  class="cguText">L’édition et la direction de la publication du site http://jeuxolypmpique/ est assurée par Ethan Delannoy, domicilié 18 rue d'Arras.
+        Numéro de téléphone est 0624446061
+        Adresse e-mail ethan.delannoy12@gmail.com.
+
+        L'hébergeur du site http://jeuxolypmpique/ est la société X, dont le siège social est situé au X, avec le numéro de téléphone : X.
+    </p>
+
+    <p class="titreCgu">Article 2 : Accès au site</p>
+
+    <p  class="cguText">Le site http://jeuxolypmpique/ permet à l'Utilisateur un accès gratuit aux services suivants :
+        Le site internet propose les services suivants :
+        Informations sur tous le déroulé des jeux olympique d'handball 2024
+        Le site est accessible gratuitement en tout lieu à tout Utilisateur ayant un accès à Internet. Tous les frais supportés par l'Utilisateur pour accéder au service (matériel informatique, logiciels, connexion Internet, etc.) sont à sa charge.
+    </p>
+
+    <p  class="cguText">L’Utilisateur non membre n'a pas accès aux services réservés. Pour cela, il doit s’inscrire en remplissant le formulaire. En acceptant de s’inscrire aux services réservés, l’Utilisateur membre s’engage à fournir des informations sincères et exactes concernant son état civil et ses coordonnées, notamment son adresse email.</p>
+
+    <p  class="cguText">Pour accéder aux services, l’Utilisateur doit ensuite s'identifier à l'aide de son identifiant et de son mot de passe qui lui seront communiqués après son inscription.
+        Tout Utilisateur membre régulièrement inscrit pourra également solliciter sa désinscription en se rendant à la page dédiée sur son espace personnel. Celle-ci sera effective dans un délai raisonnable.
+        Tout événement dû à un cas de force majeure ayant pour conséquence un dysfonctionnement du site ou serveur et sous réserve de toute interruption ou modification en cas de maintenance, n'engage pas la responsabilité de http://jeuxolypmpique/. Dans ces cas, l’Utilisateur accepte ainsi ne pas tenir rigueur à l’éditeur de toute interruption ou suspension de service, même sans préavis.
+        L'Utilisateur a la possibilité de contacter le site par messagerie électronique à l’adresse email de l’éditeur communiqué à l’Article 1.
+    </p>
+
+    <p class="titreCgu">Article 3 : Collecte des données</p>
+
+    <p  class="cguText">Le site assure à l'Utilisateur une collecte et un traitement d'informations personnelles dans le respect de la vie privée conformément à la loi n°78-17 du 6 janvier 1978 relative à l'informatique, aux fichiers et aux libertés.</p>
+
+    <p  class="cguText">En vertu de la loi Informatique et Libertés, en date du 6 janvier 1978, l'Utilisateur dispose d'un droit d'accès, de rectification, de suppression et d'opposition de ses données personnelles. L'Utilisateur exerce ce droit :
+        · via son espace personnel ;
+    </p>
+
+    <p class="titreCgu">Article 4 : Propriété intellectuelle</p>
+
+    <p  class="cguText">Les marques, logos, signes ainsi que tous les contenus du site (textes, images, son…) font l'objet d'une protection par le Code de la propriété intellectuelle et plus particulièrement par le droit d'auteur.</p>
+
+    <p  class="cguText">L'Utilisateur doit solliciter l'autorisation préalable du site pour toute reproduction, publication, copie des différents contenus. Il s'engage à une utilisation des contenus du site dans un cadre strictement privé, toute utilisation à des fins commerciales et publicitaires est strictement interdite.</p>
+
+    <p  class="cguText">Toute représentation totale ou partielle de ce site par quelque procédé que ce soit, sans l’autorisation expresse de l’exploitant du site Internet constituerait une contrefaçon sanctionnée par l’Article L 335-2 et suivants du Code de la propriété intellectuelle.</p>
+
+    <p  class="cguText">Il est rappelé conformément à l’Article L122-5 du Code de propriété intellectuelle que l’Utilisateur qui reproduit, copie ou publie le contenu protégé doit citer l’auteur et sa source.</p>
+
+    <p class="titreCgu">Article 5 : Responsabilité</p>
+
+    <p  class="cguText">Les sources des informations diffusées sur le site http://jeuxolypmpique/ sont réputées fiables mais le site ne garantit pas qu’il soit exempt de défauts, d’erreurs ou d’omissions.</p>
+
+    <p  class="cguText">Les informations communiquées sont présentées à titre indicatif et général sans valeur contractuelle. Malgré des mises à jour régulières, le site http://jeuxolypmpique/ ne peut être tenu responsable de la modification des dispositions administratives et juridiques survenant après la publication. De même, le site ne peut être tenue responsable de l’utilisation et de l’interprétation de l’information contenue dans ce site.
+        L'Utilisateur s'assure de garder son mot de passe secret. Toute divulgation du mot de passe, quelle que soit sa forme, est interdite. Il assume les risques liés à l'utilisation de son identifiant et mot de passe. Le site décline toute responsabilité.
+        Le site http://jeuxolypmpique/ ne peut être tenu pour responsable d’éventuels virus qui pourraient infecter l’ordinateur ou tout matériel informatique de l’Internaute, suite à une utilisation, à l’accès, ou au téléchargement provenant de ce site.
+    </p>
+
+    <p  class="cguText">La responsabilité du site ne peut être engagée en cas de force majeure ou du fait imprévisible et insurmontable d'un tiers.</p>
+
+    <p class="titreCgu">Article 6 : Liens hypertextes</p>
+
+    <p  class="cguText">Des liens hypertextes peuvent être présents sur le site. L’Utilisateur est informé qu’en cliquant sur ces liens, il sortira du site http://jeuxolypmpique/. Ce dernier n’a pas de contrôle sur les pages web sur lesquelles aboutissent ces liens et ne saurait, en aucun cas, être responsable de leur contenu.</p>
+
+    <p class="titreCgu">Article 7 : Cookies</p>
+
+    <p  class="cguText">L’Utilisateur est informé que lors de ses visites sur le site, un cookie peut s’installer automatiquement sur son logiciel de navigation.</p>
+
+    <p  class="cguText">Les cookies sont de petits fichiers stockés temporairement sur le disque dur de l’ordinateur de l’Utilisateur par votre navigateur et qui sont nécessaires à l’utilisation du site http://jeuxolypmpique/. Les cookies ne contiennent pas d’information personnelle et ne peuvent pas être utilisés pour identifier quelqu’un. Un cookie contient un identifiant unique, généré aléatoirement et donc anonyme. Certains cookies expirent à la fin de la visite de l’Utilisateur, d’autres restent.</p>
+
+    <p  class="cguText">L’information contenue dans les cookies est utilisée pour améliorer le site http://jeuxolypmpique/.</p>
+
+    <p  class="cguText">En naviguant sur le site, L’Utilisateur les accepte.
+        L’Utilisateur doit toutefois donner son consentement quant à l’utilisation de certains cookies.
+        A défaut d’acceptation, l’Utilisateur est informé que certaines fonctionnalités ou pages risquent de lui être refusées.
+        L’Utilisateur pourra désactiver ces cookies par l’intermédiaire des paramètres figurant au sein de son logiciel de navigation.
+    </p>
+
+    <p class="titreCgu">Article 8 : Droit applicable et juridiction compétente</p>
+
+    <p  class="cguText">La législation française s'applique au présent contrat. En cas d'absence de résolution amiable d'un litige né entre les parties, les tribunaux français seront seuls compétents pour en connaître.
+        Pour toute question relative à l’application des présentes CGU, vous pouvez joindre l’éditeur aux coordonnées inscrites à l’ARTICLE 1.
+    </p>
 
 </section>
 
